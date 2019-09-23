1e8e90fbe14b927172ffc4ba30288a84c7e00152
student <student@localhost>
2019-09-23 21:09:10 +0300

    executed task

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,18 @@
 public class Main {
+    public static void main(String[] args) {
+
+        Burger burger = new Burger();
+        burger.name = "Твистер оригинальный";
+        burger.price = 159;
+        burger.weight = 206;
+        burger.calorific = 222;
+        System.out.println("Название: " + burger.name);
+        System.out.println("Цена: " + burger.price);
+        System.out.println("Вес: " + burger.weight);
+        System.out.println("Калорийность: " + burger.calorific);
+
+
+
+
+    }
 }
